b11765be68ea2078e821caa601b13d65c82a6ee0
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2023-09-17 17:05:03 -0600

    Minor changes

--- grid-cell/agent.h
+++ grid-cell/agent.h
@@ -9,7 +9,12 @@
 #include <ctime>
 #include <chrono>
 #include <thread>
-#include <string> // Include string header for image path
+#include <string>
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+const int GRID_SIZE = 16;
+const int CELL_SIZE = SCREEN_WIDTH / GRID_SIZE;
 
 class Agent {
 public:
@@ -25,18 +30,18 @@ public:
 
     int** initMatrix();
 
-    // Object color
-    int redColor, blueColor, greenColor;
-
 private:
     std::vector<std::pair<int, int>> obstacles;
 
     SDL_Renderer* renderer;
     SDL_Texture* texture;
 
-    bool isMoving;
 
-    int gridMatrix[16][16];
+    bool isMoving;
+    // Counter of the samples recovered by the agents
+    bool hasObject = false;
+
+    int gridMatrix[GRID_SIZE][GRID_SIZE];
     // Positions
     int x, y;
     // Directions (movement)
@@ -45,10 +50,7 @@ private:
     int size;
     // Grid size
     int gridSize;
-    // Counter of the samples recovered by the agents
-    int objectCounter = 0;
-
-    
+   
 };
 
 void drawGrid(SDL_Renderer* renderer);
