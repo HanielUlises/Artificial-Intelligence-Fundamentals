0fe499d539977dd69385b0a5ad2eb4169c4c97fc
Haniel Ulises <88525113+HanielUlises@users.noreply.github.com>
2025-05-21 23:46:06 -0600

    Removing a bit of dust

--- Uninformed Search/puzzle/puzzle.cpp
+++ Uninformed Search/puzzle/puzzle.cpp
@@ -1,76 +1,65 @@
 #include "puzzle.h"
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <ctime>
 
 int puzzleState[PUZZLE_ROWS][PUZZLE_COLS];
 SDL_Texture* puzzleTiles[PUZZLE_ROWS][PUZZLE_COLS];
 
-Board::Board(SDL_Renderer* renderer, const std::string& imagePath) {
-    this->renderer = renderer;
-
+Board::Board(SDL_Renderer* renderer, const std::string& imagePath) : renderer(renderer), texture(nullptr), lastMoveTime(0) {
     SDL_Surface* imageSurface = IMG_Load(imagePath.c_str());
-    if (imageSurface == nullptr) {
+    if (!imageSurface) {
         std::cerr << "Failed to load image: " << IMG_GetError() << std::endl;
+        return;
     }
-    else {
-  
-        texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-        SDL_FreeSurface(imageSurface);
 
-        int pieceWidth = imageSurface->w / 4; // 4x4 puzzle aka 15-puzzle
-        int pieceHeight = imageSurface->h / 4;
+    texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+    int pieceWidth = imageSurface->w / 4;
+    int pieceHeight = imageSurface->h / 4;
 
-        for (int i = 0; i < PUZZLE_ROWS; ++i) {
-            for (int j = 0; j < PUZZLE_COLS; ++j) {
-                SDL_Rect srcRect = { j * pieceWidth, i * pieceHeight, pieceWidth, pieceHeight };
-                SDL_Texture* pieceTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, pieceWidth, pieceHeight);
-                SDL_SetRenderTarget(renderer, pieceTexture);
-                SDL_RenderCopy(renderer, texture, &srcRect, NULL);
-                SDL_SetRenderTarget(renderer, nullptr);
-
-                puzzleTiles[i][j] = pieceTexture;
-                puzzleState[i][j] = i * PUZZLE_COLS + j;
-            }
+    for (int i = 0; i < PUZZLE_ROWS; ++i) {
+        for (int j = 0; j < PUZZLE_COLS; ++j) {
+            SDL_Rect srcRect = { j * pieceWidth, i * pieceHeight, pieceWidth, pieceHeight };
+            SDL_Texture* pieceTexture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, pieceWidth, pieceHeight);
+            SDL_SetRenderTarget(renderer, pieceTexture);
+            SDL_RenderCopy(renderer, texture, &srcRect, nullptr);
+            SDL_SetRenderTarget(renderer, nullptr);
+            puzzleTiles[i][j] = pieceTexture;
+            puzzleState[i][j] = i * PUZZLE_COLS + j; // 0 to 15, 15 is empty
         }
-
-        ShufflePuzzle();
     }
+    SDL_FreeSurface(imageSurface);
+    ShufflePuzzle();
 }
 
 Board::~Board() {
-	if (texture != nullptr) {
-		SDL_DestroyTexture(texture);
-		texture = nullptr;
-	}
+    if (texture) SDL_DestroyTexture(texture);
+    for (int i = 0; i < PUZZLE_ROWS; ++i) {
+        for (int j = 0; j < PUZZLE_COLS; ++j) {
+            if (puzzleTiles[i][j]) SDL_DestroyTexture(puzzleTiles[i][j]);
+        }
+    }
 }
 
 void Board::ShufflePuzzle() {
     srand(static_cast<unsigned int>(time(nullptr)));
-
-    const int numShuffleMoves = 1000; 
-    int emptyRow = 3;  // Initial position of the empty space
+    const int numShuffleMoves = 1000;
+    int emptyRow = 3;
     int emptyCol = 3;
 
     for (int i = 0; i < numShuffleMoves; ++i) {
         int direction = rand() % 4;
-
- 
         int newRow = emptyRow;
         int newCol = emptyCol;
 
-        if (direction == 0 && emptyRow > 0) {
-            newRow = emptyRow - 1; // Move up
-        }
-        else if (direction == 1 && emptyRow < 3) {
-            newRow = emptyRow + 1; // Move down
-        }
-        else if (direction == 2 && emptyCol > 0) {
-            newCol = emptyCol - 1; // Move left
-        }
-        else if (direction == 3 && emptyCol < 3) {
-            newCol = emptyCol + 1; // Move right
-        }
+        if (direction == 0 && emptyRow > 0) newRow = emptyRow - 1; // Up
+        else if (direction == 1 && emptyRow < 3) newRow = emptyRow + 1; // Down
+        else if (direction == 2 && emptyCol > 0) newCol = emptyCol - 1; // Left
+        else if (direction == 3 && emptyCol < 3) newCol = emptyCol + 1; // Right
 
         std::swap(puzzleState[emptyRow][emptyCol], puzzleState[newRow][newCol]);
-
         emptyRow = newRow;
         emptyCol = newCol;
     }
@@ -79,7 +68,7 @@ void Board::ShufflePuzzle() {
 void Board::FindEmptyTile(int& row, int& col) {
     for (int i = 0; i < PUZZLE_ROWS; ++i) {
         for (int j = 0; j < PUZZLE_COLS; ++j) {
-            if (puzzleState[i][j] == 0) {
+            if (puzzleState[i][j] == 15) {
                 row = i;
                 col = j;
                 return;
@@ -91,27 +80,15 @@ void Board::FindEmptyTile(int& row, int& col) {
 void Board::MoveEmptySpace(Direction direction) {
     int emptyRow, emptyCol;
     FindEmptyTile(emptyRow, emptyCol);
-
     int newRow = emptyRow;
     int newCol = emptyCol;
 
-    if (direction == Direction::UP && emptyRow > 0) {
-        newRow = emptyRow - 1;
-    }
-    else if (direction == Direction::DOWN && emptyRow < PUZZLE_ROWS - 1) {
-        newRow = emptyRow + 1;
-    }
-    else if (direction == Direction::LEFT && emptyCol > 0) {
-        newCol = emptyCol - 1;
-    }
-    else if (direction == Direction::RIGHT && emptyCol < PUZZLE_COLS - 1) {
-        newCol = emptyCol + 1;
-    }
+    if (direction == Direction::UP && emptyRow > 0) newRow = emptyRow - 1;
+    else if (direction == Direction::DOWN && emptyRow < PUZZLE_ROWS - 1) newRow = emptyRow + 1;
+    else if (direction == Direction::LEFT && emptyCol > 0) newCol = emptyCol - 1;
+    else if (direction == Direction::RIGHT && emptyCol < PUZZLE_COLS - 1) newCol = emptyCol + 1;
 
-    // Swaps the positions of the empty tile and the selected tile
     std::swap(puzzleState[emptyRow][emptyCol], puzzleState[newRow][newCol]);
-    emptyRow = newRow;
-    emptyCol = newCol;
 }
 
 void Board::Draw(SDL_Renderer* renderer) {
@@ -121,10 +98,8 @@ void Board::Draw(SDL_Renderer* renderer) {
     for (int i = 0; i < PUZZLE_ROWS; ++i) {
         for (int j = 0; j < PUZZLE_COLS; ++j) {
             int tileID = puzzleState[i][j];
-
-            if (tileID != 0) {
+            if (tileID != 15) {
                 SDL_Texture* tileTexture = puzzleTiles[tileID / PUZZLE_COLS][tileID % PUZZLE_COLS];
-
                 SDL_Rect destRect = { j * tileWidth, i * tileHeight, tileWidth, tileHeight };
                 SDL_RenderCopy(renderer, tileTexture, nullptr, &destRect);
             }
@@ -132,12 +107,9 @@ void Board::Draw(SDL_Renderer* renderer) {
     }
 }
 
-
 void handleEvents(SDL_Event& e, bool& quit, Board& board) {
     while (SDL_PollEvent(&e)) {
-        if (e.type == SDL_QUIT) {
-            quit = true;
-        }
+        if (e.type == SDL_QUIT) quit = true;
         else if (e.type == SDL_KEYDOWN) {
             switch (e.key.keysym.sym) {
             case SDLK_UP:
@@ -154,33 +126,123 @@ void handleEvents(SDL_Event& e, bool& quit, Board& board) {
                 break;
             case SDLK_ESCAPE:
                 quit = true;
+                break;
+            case SDLK_s:
+                Puzzle solver;
+                board.solutionPath = solver.solve(puzzleState);
+                board.lastMoveTime = SDL_GetTicks();
+                break;
             }
-            
         }
     }
 }
 
 void runProgram() {
-	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("Puzzle BFS/DFS", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << std::endl;
+        return;
+    }
+    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        std::cerr << "IMG_Init failed: " << IMG_GetError() << std::endl;
+        return;
+    }
 
-	SDL_Event e;
+    SDL_Window* window = SDL_CreateWindow("15-Puzzle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (!window) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << std::endl;
+        IMG_Quit();
+        SDL_Quit();
+        return;
+    }
 
-	bool quit = false;
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "SDL_CreateRenderer failed: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        IMG_Quit();
+        SDL_Quit();
+        return;
+    }
 
-	Board newBoard(renderer, "image/gatito.png");
-	while (!quit) {
-		handleEvents(e, quit, newBoard);
+    SDL_Event e;
+    bool quit = false;
+    Board board(renderer, "image/gatito.png");
 
-		SDL_RenderClear(renderer);
+    while (!quit) {
+        handleEvents(e, quit, board);
+        if (!board.solutionPath.empty() && SDL_GetTicks() - board.lastMoveTime > 500) {
+            Board::Direction dir = board.solutionPath.front();
+            board.solutionPath.erase(board.solutionPath.begin());
+            board.MoveEmptySpace(dir);
+            board.lastMoveTime = SDL_GetTicks();
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        board.Draw(renderer);
+        SDL_RenderPresent(renderer);
+    }
 
-		newBoard.Draw(renderer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    IMG_Quit();
+    SDL_Quit();
+}
 
-		SDL_RenderPresent(renderer);
-	}
+std::vector<Board::Direction> Puzzle::solve(const int initialState[PUZZLE_ROWS][PUZZLE_COLS]) {
+    std::string initialString;
+    for (int i = 0; i < PUZZLE_ROWS; ++i) {
+        for (int j = 0; j < PUZZLE_COLS; ++j) {
+            initialString += static_cast<char>(initialState[i][j]);
+        }
+    }
 
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+    std::string goal;
+    for (int i = 0; i < 16; ++i) goal += static_cast<char>(i);
+
+    std::queue<std::string> q;
+    std::unordered_set<std::string> visited;
+    std::unordered_map<std::string, std::pair<std::string, Board::Direction>> parent;
+
+    q.push(initialString);
+    visited.insert(initialString);
+    parent[initialString] = {"", Board::Direction::UP};
+
+    while (!q.empty()) {
+        std::string current = q.front();
+        q.pop();
+        if (current == goal) {
+            std::vector<Board::Direction> path;
+            while (parent[current].first != "") {
+                path.push_back(parent[current].second);
+                current = parent[current].first;
+            }
+            std::reverse(path.begin(), path.end());
+            return path;
+        }
+
+        size_t pos = current.find(static_cast<char>(15));
+        int row = pos / PUZZLE_COLS;
+        int col = pos % PUZZLE_COLS;
+
+        std::vector<std::pair<int, int>> moves = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        std::vector<Board::Direction> directions = {
+            Board::Direction::UP, Board::Direction::DOWN,
+            Board::Direction::LEFT, Board::Direction::RIGHT
+        };
+
+        for (int d = 0; d < 4; ++d) {
+            int newRow = row + moves[d].first;
+            int newCol = col + moves[d].second;
+            if (newRow >= 0 && newRow < PUZZLE_ROWS && newCol >= 0 && newCol < PUZZLE_COLS) {
+                std::string newState = current;
+                std::swap(newState[row * PUZZLE_COLS + col], newState[newRow * PUZZLE_COLS + newCol]);
+                if (visited.find(newState) == visited.end()) {
+                    visited.insert(newState);
+                    q.push(newState);
+                    parent[newState] = {current, directions[d]};
+                }
+            }
+        }
+    }
+    return {};
 }
--- Uninformed Search/puzzle/puzzle.h
+++ Uninformed Search/puzzle/puzzle.h
@@ -1,88 +1,37 @@
 #pragma once
-#include "iostream"
-#include "vector"
-#include "cstdlib"
-#include "string.h"
-
-#include "SDL.h"
-#include "SDL_image.h"
-
-#undef main
+#include <iostream>
+#include <vector>
+#include <string>
+#include <SDL.h>
+#include <SDL_image.h>
 
 const int SCREEN_WIDTH = 1000;
 const int SCREEN_HEIGHT = 1000;
-
 const int PUZZLE_ROWS = 4;
 const int PUZZLE_COLS = 4;
 
-// Stack data structure that stores the paths
-struct Stack {
-	std::vector<std::string> paths;
-
-	std::string top() const {
-		if (!paths.empty()) {
-			return paths.back();
-		}
-		else {
-			throw std::runtime_error("Stack is empty\n");
-		}
-	}
-
-	bool isEmpty() const {
-		return paths.empty();
-	}
-
-	void push(const std::string& path) {
-		paths.push_back(path);
-	}
-
-	void pop() {
-		if (!(paths.empty())) {
-			paths.pop_back();
-		}
-		else {
-			throw std::runtime_error("Stack is empty\n");
-		}
-	}
-
-	size_t size() const {
-		return paths.size();
-	}
-};
-
-// Here is the definition of the puzzle to be solved
-// It can be solved by BFS of DFS
 class Puzzle {
 public:
-
+    std::vector<Board::Direction> solve(const int initialState[PUZZLE_ROWS][PUZZLE_COLS]);
 };
 
-// The window to be displayed with the sub-images
 class Board {
 public:
-	enum class Direction {
-		UP,
-		DOWN,
-		LEFT,
-		RIGHT
-	};
+    enum class Direction { UP, DOWN, LEFT, RIGHT };
+    Board(SDL_Renderer* renderer, const std::string& imagePath);
+    ~Board();
+    void Draw(SDL_Renderer* renderer);
+    void ShufflePuzzle();
+    void FindEmptyTile(int& row, int& col);
+    void MoveEmptySpace(Direction direction);
 
-	Board(SDL_Renderer * renderer, const std::string &imagePath);
-	~Board();
+    std::vector<Direction> solutionPath;
+    Uint32 lastMoveTime;
 
-	void Draw(SDL_Renderer* renderer);
-	void ShufflePuzzle();
-	void FindEmptyTile(int& row, int& col);
-	void MoveEmptySpace(Direction direction);
-private: 
-	
-
-	int emptyRow;
-	int emptyCol;
-
-	SDL_Renderer* renderer;
-	SDL_Texture* texture;
+private:
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
 };
 
-void handleEvents();
+void handleEvents(SDL_Event& e, bool& quit, Board& board);
 void runProgram();
